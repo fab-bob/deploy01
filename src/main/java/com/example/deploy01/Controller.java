4c54c3ff611fa6fcc8dff45e6a5d74f89d2e487d
Fabrizio Lagioia <meandi958@gmail.com>
2024-06-17 15:38:27 +0200

    corretta variabile d'ambiente

--- src/main/java/com/example/deploy01/Controller.java
+++ src/main/java/com/example/deploy01/Controller.java
@@ -1,5 +1,7 @@
 package com.example.deploy01;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+    @Autowired
+    Environment environment;
+
     @GetMapping
-    public String yourName (@RequestParam String devName) {
-        return devName;
+    public String yourName () {
+        return environment.getProperty("devName");
     }
 }
